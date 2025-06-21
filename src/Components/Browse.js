f8d32a8f71134eeadae51cca04d921ff92ebedc6
setupndt <sarthakpandit802@gmail.com>
2025-06-21 22:18:05 +0530

    build sign up and sign in page

---
+++ src/Components/Browse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Browse = () => {
+  return (
+    <div>
+     <h1 className='text-black'>Hello</h1>
+    </div>
+  )
+}
+
+export default Browse;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ Correct content paths
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
